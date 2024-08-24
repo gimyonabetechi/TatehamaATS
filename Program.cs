28d9ba6d67befcaf648b1db4485199313f130069
TakumiteTudo <takumit2886@gmail.com>
2024-08-25 06:09:57 +0900

    終了時タヌ電へ通知

--- Program.cs
+++ Program.cs
@@ -11,7 +11,17 @@ namespace TatehamaATS
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+            // アプリケーション終了時に実行するイベントハンドラーを登録
+            Application.ApplicationExit += new EventHandler(OnApplicationExit);
+
             Application.Run(new MainWindow());
+        }
+        /// <summary>
+        /// アプリケーション終了時に実行されるメソッド。
+        /// </summary>
+        static void OnApplicationExit(object sender, EventArgs e)
+        {
+            MainWindow.transfer.SendDeleteAsync();
         }
     }
 }
